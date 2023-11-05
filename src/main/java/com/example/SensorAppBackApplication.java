7d26dac70230070a97b9b10d43c5388ad4cfd604
alex-den <gizmo5024@yandex.ru>
2023-11-05 11:41:45 +0300

    init commit

---
+++ src/main/java/com/example/SensorAppBackApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootApplication
+public class SensorAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorAppBackApplication.class, args);
+	}
+
+}
